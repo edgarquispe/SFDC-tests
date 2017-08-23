98d985c431e00635659ab2865a45f2eaba98afed
yuryver <yury.ortuno@fundacion-jala.org>
2017-08-22 20:03:18 -0400

    corrections spaces of 2 a 4 spaces and add RuntimeException

--- src/main/java/org/fundacionjala/sfdc/core/driver/SauceLabs.java
+++ src/main/java/org/fundacionjala/sfdc/core/driver/SauceLabs.java
@@ -1,14 +1,14 @@
 package org.fundacionjala.sfdc.core.driver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.fundacionjala.sfdc.core.Env;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 /**
  * SauceLabs class that implements IBrowsers.
@@ -50,6 +50,7 @@ public class SauceLabs implements IBrowser {
             driver = new RemoteWebDriver(new URL(URL), setCapabilities());
         } catch (MalformedURLException e) {
             LOGGER.error("Not instance driver");
+            throw new RuntimeException();
         }
         return driver;
     }
