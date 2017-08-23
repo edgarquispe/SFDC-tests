2d70da298401525803e6ef21f92a03e1347a0009
Abel <abelbs27101@gmail.com>
2017-08-22 20:02:06 -0400

    Adding RunTime Exception

--- src/main/java/org/fundacionjala/sfdc/core/driver/DockerFirefoxBrowser.java
+++ src/main/java/org/fundacionjala/sfdc/core/driver/DockerFirefoxBrowser.java
@@ -5,11 +5,12 @@ import java.net.URL;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.fundacionjala.sfdc.core.Env;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import org.fundacionjala.sfdc.core.Env;
 
 /**
  * DockerChromeBrowser class that implements IBrowsers.
@@ -36,6 +37,7 @@ public class DockerFirefoxBrowser implements IBrowser {
 
         } catch (MalformedURLException e) {
             LOGGER.error(e.getMessage());
+            throw new RuntimeException();
         }
         return driver;
     }
