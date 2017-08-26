9c8b1d3e617257a64f8ad2df4b3c972cb97916c1
yuryver <yury.ortuno@fundacion-jala.org>
2017-08-26 19:08:54 -0400

    fix create opportunity

--- src/main/java/org/fundacionjala/sfdc/pages/base/HomeBase.java
+++ src/main/java/org/fundacionjala/sfdc/pages/base/HomeBase.java
@@ -1,13 +1,11 @@
 package org.fundacionjala.sfdc.pages.base;
 
-import org.fundacionjala.sfdc.core.CommonActions;
-import org.fundacionjala.sfdc.core.driver.DriverManager;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.fundacionjala.sfdc.core.CommonActions;
+import org.fundacionjala.sfdc.core.driver.DriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
