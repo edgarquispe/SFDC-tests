9d2b126ffa8cb675779c23ee4695e7c5fbf50c56
marcos-x86 <marcos.x86@outlook.com>
2017-08-28 17:11:51 -0400

    Error Message Assertion method moved

--- src/test/java/org/fundacionjala/sfdc/stepdefinitions/CommonAssertionSteps.java
+++ src/test/java/org/fundacionjala/sfdc/stepdefinitions/CommonAssertionSteps.java
@@ -1,10 +1,15 @@
 package org.fundacionjala.sfdc.stepdefinitions;
 
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 import org.testng.asserts.Assertion;
 import org.testng.asserts.SoftAssert;
 
 import org.fundacionjala.sfdc.entities.Helper;
+import org.fundacionjala.sfdc.pages.Navigator;
+import org.fundacionjala.sfdc.pages.SObject;
+
+import static org.testng.Assert.assertTrue;
 
 /**
  * Common assert step definitions.
@@ -34,5 +39,17 @@ public class CommonAssertionSteps {
         if (assertion instanceof SoftAssert) {
             ((SoftAssert) assertion).assertAll();
         }
+    }
+
+
+    /**
+     * Assertion step to verify the error message displayed.
+     *
+     * @param msg     error message that should be displayed.
+     * @param sObject SObject enum parameter.
+     */
+    @Then("^\"([^\"]*)\" message should be displayed in \"([^\"]*)\" form$")
+    public void messageShouldBeDisplayedInForm(String msg, SObject sObject) {
+        assertion.assertTrue(Navigator.mapForm(sObject).errorNotificationText().contains(msg));
     }
 }
