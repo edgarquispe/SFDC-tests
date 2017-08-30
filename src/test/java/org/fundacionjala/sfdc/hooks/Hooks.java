e96fa26ac4d8d7a65ea99912f46ae946e08c1de2
Jose Torrez <Jose.Torrez@fundacion-jala.org>
2017-08-30 18:37:41 -0400

    Snapshots are taken when a scenario fails

--- src/test/java/org/fundacionjala/sfdc/hooks/Hooks.java
+++ src/test/java/org/fundacionjala/sfdc/hooks/Hooks.java
@@ -1,13 +1,16 @@
 package org.fundacionjala.sfdc.hooks;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
-import org.testng.asserts.SoftAssert;
-
 import org.fundacionjala.sfdc.core.CommonActions;
 import org.fundacionjala.sfdc.core.driver.DriverManager;
 import org.fundacionjala.sfdc.entities.Helper;
 import org.fundacionjala.sfdc.pages.Navigator;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.asserts.SoftAssert;
 
 /**
  * Hooks Class for actions to run before and after Scenarios.
@@ -79,6 +82,20 @@ public class Hooks {
     @After(order = 5)
     public void cleanHelper() {
         helper = new Helper();
+    }
+
+    /**
+     * Takes a snapshot when a scenario fails.
+     *
+     * @param scenario variable for Cucumber features.
+     */
+    @After
+    public void takeScreenShot(Scenario scenario) {
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) DriverManager.getInstance().getDriver())
+                    .getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png"); //stick it in the report
+        }
     }
 
     /**
